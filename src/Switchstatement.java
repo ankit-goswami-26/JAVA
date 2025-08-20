6be3b30e96c2a5549b0a0d80bfecbc4ce1318eec
ankit-goswami-26 <goswamiankit459@gmail.com>
2025-08-20 09:53:50 +0530

    Java Basics

---
+++ src/Switchstatement.java
@@ -0,0 +1,52 @@
+/* public class Switchstatement {
+    public static void main(String[] args) {
+        String day = "Thursday";
+
+        String result = switch (day) {
+            case "Saturday", "Sunday" -> "6am";
+            case "Monday" -> "7am";
+            default -> "8am";
+        };
+
+        System.out.println(result);
+    }
+}*/
+
+
+
+
+
+
+
+// INSTEAD OF USING SYSTEM.OUT.PRINTLN LIKE A OLD METHOD OR MULTIPLE ASSIGNING VALUE
+// THE NEW AND BEST POSSIBLE WAY IS USING STRING RESULT VARIABLE
+// ITS AN NEW METHOD FOR SWITCH STATEMENT
+// NEW METHOD REPLACES BREAKS WITH A RIGHT ARROW
+//IF YOU DON'T WANT TO USE ARROW, THEN YOU CAN USE COLON BUT YOU NEED TO MENTION YIELD
+// Since it's an expression you need to put semicolon in the end
+
+
+
+
+
+
+
+//        switch(day) // ITS AN OLD METHOD OF SWITCH STATEMENT
+//        {
+//            case "Saturday", "Sunday" :
+//                System.out.println("6am");
+//                break;
+//
+//
+//            case "Monday" :
+//                System.out.println("7am");
+//                break;
+//
+//
+//            default:
+//                System.out.println("8am");
+//
+//        }
+//    }
+//   }
+
