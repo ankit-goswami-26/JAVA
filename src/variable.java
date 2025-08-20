340de42726a05daa6dbdbf5a1ea3c30454169191
ankit-goswami-26 <goswamiankit459@gmail.com>
2025-08-20 16:38:14 +0530

    Java Basics

---
+++ src/variable.java
@@ -0,0 +1,88 @@
+
+// MY SECOND CODE
+
+public class variable {
+    public static void main(String[] args){
+        /*
+
+    Variable = a reusable container for a value
+               a variable behaves as if it was the value it contains
+
+     */
+
+    /*
+
+    primitive = simple values stored directly in memory (stack)
+    reference = memory address (stack) that points to the (heap)
+
+    primitive vs reference
+    ---------    ---------
+    int          string
+    double       array
+    char         object
+    boolean
+
+    2 steps to create a variable
+    ----------------------------
+    1) DECLARATION
+    2) ASSIGNMENT
+
+     */
+/*
+       int age = 20;
+        int money = 100;
+        int year = 2025;
+        System.out.println(age);
+        System.out.println(year);
+        System.out.println("the year is " + year);
+        System.out.println("the next year is " + year);
+        double price = 19.0;
+        double gpa = 20.0;
+        double temp = -10.0;
+
+        System.out.println("$"+ price);
+        System.out.println("The gpa is "+ gpa);
+        System.out.println("the Temperature is "+ temp);
+        char name = 'R';
+        char dollar = '$';
+        char ex = '!';
+
+        System.out.println(dollar);
+        boolean isStudent = true;
+        boolean forStudent = false;
+        boolean isOnline = true;
+        System.out.println(isOnline);
+        if(isOnline){
+            System.out.println("lets consider them as students");
+        }
+        else{
+            System.out.println("lets consider them as animals");
+        }
+        String name = "my name is Ankit";
+        String year = " the year is 2025";
+        System.out.println("At the age of " + age +" "+ "I earned almost " + dollar + money + " " + "in" +year);
+        if(isStudent){
+            System.out.println("i have done right");
+        }
+        else{
+            System.out.println("i have done something wrong");
+        }
+    String name = "bro code";
+    int gta = 6;
+    double pi = 3.14159;
+    char gender = 'm';
+*/
+
+        boolean isAdmin = false;
+
+
+        if(isAdmin){
+            System.out.println("This is true brooooooooooo");
+        }
+        else{
+            System.out.println("This is not true broooooooo");
+        }
+
+
+    }
+}
