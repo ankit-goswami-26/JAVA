4b9b70b2c8dd28b680c7f877b14934c1f0405522
ankit-goswami-26 <goswamiankit459@gmail.com>
2025-08-21 21:25:54 +0530

    Java Basics

---
+++ src/CompoundIn.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class CompoundIn {
+    public static void main(String[] args){
+
+        //compound interest calculator
+        Scanner scanner = new Scanner(System.in);
+
+        double principle;
+        double rate;
+        int timesCompounded;
+        int years;
+        double amount;
+
+        System.out.print("Enter the principal amount: ");
+        principle = scanner.nextDouble();
+
+        System.out.print("Enter the interest rate(in %):");
+        rate = scanner.nextDouble() / 100;
+
+        System.out.print("Enter the # of times compounded per year: ");
+        timesCompounded = scanner.nextInt();
+
+        System.out.print("Enter the # of years: ");
+        years = scanner.nextInt();
+
+        amount = principle * Math.pow(1 + rate / timesCompounded, timesCompounded * years); //Formula
+
+        System.out.printf("The amount after %d years is $%.2f",years , amount);
+
+        scanner.close();
+    }
+}
