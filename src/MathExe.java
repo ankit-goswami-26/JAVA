3b667e6a1cb88ca0ede3f83b3bd600629a8d28dd
ankit-goswami-26 <goswamiankit459@gmail.com>
2025-08-21 16:41:23 +0530

    Java Basics

---
+++ src/MathExe.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class MathExe {
+    public static void main(String[] args){
+
+//        HYPOTENUSE
+        Scanner scanner = new Scanner(System.in);
+
+        double a;
+        double b;
+        double c;
+
+        System.out.print("Enter the length of side A: ");
+        a = scanner.nextDouble();
+
+        System.out.print("Enter the length of side B: ");
+        b = scanner.nextDouble();
+
+        c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+
+        System.out.printf("The Hypotenuse (side c):%.1fcm²",c );
+        // For power square....
+        // Windows: Numlock is on + Alt + 0178  or  Mac: Control + Command + Space
+
+        // for power cube....
+        // Windows: Numblock is on + Alt + 0179
+
+        scanner.close();
+    }
+}
+
+        /* MATH CONSTANTS
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+        */
+
+
+
+        /* MATH METHODS
+        result = Math.pow(2, 5);
+        result = Math.abs(-5);
+        result = Math.sqrt(9);
+        result = Math.round(3.14);
+        result = Math.ceil(3.14);
+        result = Math.floor(3.99);
+        result = Math.max(10, 20);
+        result = Math.min(10, 20);
+
+        */
