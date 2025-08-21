1f83e714343d4b0808abf13fc9b8af07c09e58ab
ankit-goswami-26 <goswamiankit459@gmail.com>
2025-08-21 14:11:33 +0530

    Java Basics

---
+++ src/RandomNumberGen.java
@@ -0,0 +1,22 @@
+
+
+import java.util.Random;
+
+public class RandomNumberGen {
+    public static void main(String[] args){
+
+        Random random = new Random();
+
+        boolean isHEADS;
+
+
+        isHEADS = random.nextBoolean();
+
+        if(isHEADS){
+            System.out.println("HEADS");
+        }
+        else{
+            System.out.println("TAILS");
+        }
+    }
+}
