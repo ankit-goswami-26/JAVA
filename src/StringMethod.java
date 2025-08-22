f49dfeae748b20830aeb832296f1b568492bc0f8
ankit-goswami-26 <goswamiankit459@gmail.com>
2025-08-22 07:48:09 +0530

    Java Basics

---
+++ src/StringMethod.java
@@ -0,0 +1,46 @@
+public class StringMethod {
+    public static void main(String[] args){
+
+        String name = "Bro code";
+        /*
+
+
+        int length = name.length();
+        char letter = name.charAt(0);
+        int index = name.indexOf("o");
+        int lastIndex = name.lastIndexOf("o");
+
+
+        name = name.toUpperCase();
+        name = name.toLowerCase();
+        name = name.trim();
+        name = name.replace("o","a");
+
+
+        if(name.isEmpty()){
+            System.out.println("Your name isEmpty");
+        }
+        else{
+            System.out.println("Hello " + name);
+        }
+
+
+        if(name.contains("")){
+            System.out.println("Your name contains a space");
+        }
+        else{
+            System.out.println("Your name Doesn't contain a space");
+        }
+
+
+        if(name.equalsIgnoreCase("Ronny")){ // its always Case sensitive to avoid it you can use name.equalsIgnoreCase
+            System.out.println("You name matched");
+        }
+        else{
+            System.out.println("Hello " + name);
+        }
+
+        */
+
+    }
+}
