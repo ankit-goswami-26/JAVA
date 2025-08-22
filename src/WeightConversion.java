0a4eb93363b4dcebda296ebee254f9b5d3e3888b
ankit-goswami-26 <goswamiankit459@gmail.com>
2025-08-22 09:11:53 +0530

    Java Basics

---
+++ src/WeightConversion.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class WeightConversion {
+    public static void main(String[] args) {
+
+        // WEIGHT CONVERSION PROGRAM
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Declare variables
+        double weight;
+        double newWeight;
+        int choice;
+
+
+        // Welcome message
+        System.out.println("Weight Conversion Program");
+        System.out.println("1: Convert lbs to kgs");
+        System.out.println("2: Convert kgs to lbs");
+
+
+        // Prompt for UserInput
+        System.out.print("Choose an option: ");
+        choice = scanner.nextInt();
+
+
+        // option 1 convert lbs to kgs
+        if (choice == 1) {
+            System.out.print("Enter the weight in lbs: ");
+            weight = scanner.nextInt();
+            newWeight = weight * 0.453592;
+
+            System.out.printf("The new weight in kgs is: %.2f", newWeight);
+        }
+
+
+        // option 2 convert kgs to lbs
+        else if (choice == 2) {
+            System.out.print("Enter the weight in kgs: ");
+            weight = scanner.nextInt();
+            newWeight = weight * 2.20462;
+
+            System.out.printf("The new weight in lbs is: %.2f", newWeight);
+        }
+
+
+        // else print not a valid choice
+        else{
+            System.out.println("That was not valid choice!");
+        }
+        scanner.close();
+    }
+
+}
