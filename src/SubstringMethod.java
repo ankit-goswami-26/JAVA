510d8919ea8a6dbb8f7d364b30689d63c0c7be01
ankit-goswami-26 <goswamiankit459@gmail.com>
2025-08-22 08:18:31 +0530

    Java Basics

---
+++ src/SubstringMethod.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class SubstringMethod {
+    public static void main(String[] args){
+
+        // .substring() = A method used to extract a portion of a string
+        //                .substring(star, end)
+
+        Scanner scanner = new Scanner(System.in);
+
+        String email;
+        String username;
+        String domain;
+
+        System.out.print("Enter your email:");
+        email = scanner.nextLine();
+
+
+        /*
+        String email = "goswamiankit459@gmail.com";
+        String pass = email.substring(0, 15);
+        String domain = email.substring(15); // we can perform this in both ways by (15, 25)
+
+         */
+
+
+        if(email.contains("@")) {
+            username = email.substring(0, email.indexOf("@"));
+            domain = email.substring(email.indexOf("@") + 1); // it's another way to perform using index method
+
+            System.out.println(username);
+            System.out.println(domain);
+            scanner.close();
+        }
+        else{
+            System.out.println("@ symbol is missing");
+        }
+    }
+}
