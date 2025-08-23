5f31118c72ff9b730acedf4fa3cd96c818a1ace7
ankit-goswami-26 <goswamiankit459@gmail.com>
2025-08-23 11:22:18 +0530

    Java Basics

---
+++ src/TemperatureConverter.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class TemperatureConverter {
+    public static void main(String[] args){
+        // Temperature converter
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Variable Declaration
+        double temp;
+        double newTemp;
+        String unit;
+
+        // Prompt for UserInput
+        System.out.print("Enter the temperature: ");
+        temp = scanner.nextDouble();
+
+        System.out.print("Convert to Celsius or Fahrenheit? (C or F): ");
+        unit = scanner.next().toUpperCase();
+
+        // variable = (condition) ? True : False; #Ternary Operator
+        newTemp = (unit.equals("C")) ? (temp - 32) * 5 / 9 : (temp * 5 / 9 ) + 32 ;
+        System.out.printf("%.1f°%s",newTemp, unit);
+
+        scanner.close();
+    }
+}
---
+++ src/TernaryOperator.java
@@ -0,0 +1,36 @@
+public class TernaryOperator {
+    public static void main(String[] args){
+
+        //Ternary Operator ? = Return 1 or 2 values if a condition is true
+
+        // variable = (condition) ? ifTrue : ifFalse;
+
+        int score = 20;
+        int number = 20;
+
+
+//        # BY USING IF STATEMENT
+
+        /*
+        if(score >= 60){
+            System.out.println("PASS");
+        }
+        else{
+            System.out.println("FAIL");
+        }
+        */
+
+
+//        # TERNARY OPERATOR
+
+        /*
+        String passOrFail = (score >= 60) ? "PASS" : "FAIL";
+
+        System.out.println(passOrFail);
+        */
+
+        double Grade = (number >= 40) ? 10 : 90;
+
+        System.out.printf("%.0f%%\n" ,Grade );
+    }
+}
