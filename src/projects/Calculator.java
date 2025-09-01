d2b371c567dba88cc2bd40bd2c09d812251a8a8f
ankit-goswami-26 <goswamiankit459@gmail.com>
2025-09-01 23:59:01 +0530

    adding a new project

---
+++ src/projects/Calculator.java
@@ -0,0 +1,50 @@
+package projects;
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        double num1;
+        double num2;
+        char operator;
+        double result = 0;
+        boolean validOperation = true;
+
+        System.out.print("Enter the first number: ");
+        num1 = scanner.nextDouble();
+
+        System.out.print("Enter an operator (+, -, *, /, ^): ");
+        operator = scanner.next().charAt(0);
+
+        System.out.print("Enter the second number: ");
+        num2 = scanner.nextDouble();
+
+        switch(operator){
+            case '+' -> result = num1 + num2;
+            case '-' -> result = num1 - num2;
+            case '*' -> result = num1 * num2;
+            case '/' -> {
+                if(num2 == 0){
+                    System.out.print("Cannot divide by zero!");
+                    validOperation = false;
+                }
+                else{
+                    result = num1 / num2;
+                }
+            }
+            case '^' -> result = Math.pow(num1, num2);
+            default -> {
+                System.out.print("Invalid operator!");
+                validOperation = false;
+            }
+        }
+
+        if(validOperation){
+            System.out.println(result);
+        }
+
+        scanner.close();
+    }
+
+}
