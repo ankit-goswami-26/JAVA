4f2371707a989f6ba01015ecf337f750a490d7fe
ankit-goswami-26 <goswamiankit459@gmail.com>
2026-06-26 20:08:47 +0530

    Took name as input and printed a greeting message for that particular name

---
+++ src/Assignments/Greetings.java
@@ -0,0 +1,14 @@
+package Assignments;
+
+import java.util.Scanner;
+
+public class Greetings {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = input.nextLine();
+
+        System.out.println("Thank you " + name + " for joining Amity University");
+    }
+}
