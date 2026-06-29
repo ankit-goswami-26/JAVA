e5a720d18b2c6a733affbe6972d789fc7c955e24
ankit-goswami-26 <goswamiankit459@gmail.com>
2026-06-29 12:03:52 +0530

    calculating Fibonacci Series up to n numbers.

--- src/Assignments/Fibonacci.java
+++ src/Assignments/Fibonacci.java
@@ -26,3 +26,34 @@ public class Fibonacci {
         input.close();
     }
 }
+
+
+
+
+// Using While loop
+
+//public static void main(String[] args) {
+//    Scanner input = new Scanner(System.in);
+//
+//    System.out.print("Enter the number of Fibonacci series: ");
+//    int n = input.nextInt();
+//
+//    if (n <= 0){
+//        System.out.println("Number must be positive");
+//        return;
+//    }
+//
+//    int a = 0, b = 1;
+//    int count = 2;
+//
+//    while (count <= n){
+//        int temp = a + b;
+//        a = b;
+//        b= temp;
+//        count++;
+//    }
+//
+//    System.out.println(b);
+//
+//    input.close();
+//}
