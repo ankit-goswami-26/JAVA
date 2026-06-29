65815695f0d35a3a93cef3a087c8397932b71dc2
ankit-goswami-26 <goswamiankit459@gmail.com>
2026-06-29 12:45:22 +0530

    Finding total count of n number using while loop

---
+++ src/Assignments/CountNums.java
@@ -0,0 +1,18 @@
+package Assignments;
+
+public class CountNums {
+    public static void main(String[] args) {
+
+        int n = 123252;
+        int count = 0;
+
+        while (n > 0){
+            int rem = n % 10;
+            if (rem == 2){
+                count++;
+            }
+            n /= 10;
+        }
+        System.out.println(count);
+    }
+}
