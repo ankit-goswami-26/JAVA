746fe5a63b5af2394ce99d0259fd7db779487481
ankit-goswami-26 <goswamiankit459@gmail.com>
2026-07-03 05:58:45 +0530

    Whether no. is prime or not using functions

---
+++ src/Assignments/Prime.java
@@ -0,0 +1,31 @@
+package Assignments;
+
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = input.nextInt();
+
+        System.out.println(isPrime(n));
+    }
+
+    static boolean isPrime(int n) {
+        if (n <= 1){
+            return false;
+        }
+        
+        int c = 2;
+        while (c * c <= n ){
+            if (n % c == 0){
+                return false;
+            }
+            c++;
+        }
+
+        return c * c > n;
+    }
+}
